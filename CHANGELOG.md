9ff638d9da04f498a6b3f55084246d3246ccbf23
gmpassos <gracilianomp@gmail.com>
2023-08-15 04:42:58 -0300

    v1.0.10
    - `data_serializer_io.dart`: `data_serializer.io`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.10
+
+- `data_serializer_io.dart`: `data_serializer.io`
+
 ## 1.0.9
 
 - `BytesIO`:
---
+++ lib/data_serializer_io.dart
@@ -0,0 +1,5 @@
+/// Library to handle data serialization/deserialization efficiently with `dart:io` support.
+library data_serializer.io;
+
+export 'data_serializer.dart';
+export 'src/bytes_io_file.dart';
